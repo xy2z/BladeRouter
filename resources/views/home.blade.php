41f13a8f3dcfc3945741a2cb6b9c90217e972abc
xy2z <xyzbk89@gmail.com>
2018-09-18 20:43:39 +0200

    Now using BladerCore

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 	<h1>Welcome to Blader!</h1>
-	Feel free to delete all views and start creating your project.<br />
+	Feel free to remove all views and start creating your project.<br />
 	<br />
 	Enjoy!
 @endsection
